7f2d81d1d966e907d538005fbed9297d83d069dd
JonayGodoy <jonay.godoy.reyes@gmail.com>
2019-01-05 19:14:26 +0100

    fix menu Mobile

--- src/components/commons/menuMobile/MenuMobile.js
+++ src/components/commons/menuMobile/MenuMobile.js
@@ -1,13 +1,12 @@
 import React from 'react';
-/*
-import Link  from "gatsby-link";
-import config from "../../../../config-blog"
-import * as routesPath from "../../../routePath";
-*/
+
+import { NavLink } from 'react-router-dom';
+import config from "../../../config-blog"
+import * as routesPath from "../../../pages/routePath";
 
 import './menuMobile.css';
-//import img_head from '../../../assets/img/head.jpg';
-//import svg_menu_hamburger from '../../../assets/img/menu-hamburger.svg';
+import img_head from '../../../assets/img/head.jpg';
+import svg_menu_hamburger from '../../../assets/img/menu-hamburger.svg';
 
 
 class MenuMobile extends React.Component {
@@ -29,32 +28,30 @@ class MenuMobile extends React.Component {
     }
 
     render(){
-      return(<div>insertar menuMobile</div>);
-      /*
         return (
             <div className="hidden-header-mobile">
 
                 <div className="head-mobile">
                     <img className="btn-hamburger" src={svg_menu_hamburger} onClick={this.toggle} />
-                    <Link to={'/'} className="title-header-mobile">
+                    <NavLink to={'/'} className="title-header-mobile">
                         <h2 >{config.blogTitle}</h2>
-                    </Link>
+                    </NavLink>
                 </div>
                 <div id="drawer" className={ this.state.isHidden ? "" : "background-menu-mobile"}
                      onClick={this.toggle}>
                     <div className="drawer-panel panel-new"
                          style={this.state.isHidden ? { left: "-500px"} :{left: "0" }}>
                         <div>
-                            <Link to={'/'} onClick={this.toggle}>
+                            <NavLink to={'/'} onClick={this.toggle}>
                                 <div className="img_logo_mobile" style={ { backgroundImage: `url(${img_head})`} } />
-                            </Link>
+                            </NavLink>
                             <div onClick={this.toggle}>
-                                {routesPath.menu.map((routePath, index) =>(
+                                {routesPath.routes.map((routePath, index) =>(
                                     <div key={index}>
                                         <hr/>
-                                        <Link to={routePath.path}>
+                                        <NavLink to={routePath.path}>
                                             <h3 className="blog-nav-item-mobile">{routePath.name}</h3>
-                                        </Link>
+                                        </NavLink>
                                     </div>
                                 ))}
                             </div>
@@ -63,8 +60,6 @@ class MenuMobile extends React.Component {
                 </div>
             </div>
         );
-        */
-
     }
 }
 
