ceb0f65c53d4e39bf47ddda7ea8e5bdc1a85eb81
JonayGodoy <jonay.godoy.reyes@gmail.com>
2019-01-07 15:17:20 +0100

    add comment

---
+++ src/utils/articlePagesGenerator/ArticlePageGenertor.specT.js
@@ -0,0 +1,12 @@
+/*
+crear tests del generador de articulos
+import ArticlePagesGenerator from "./ArticlePagesGenerator";
+
+describe("ArticlePagesGenerator",() =>{
+  it("return convert raw tags in tag", () => {
+    const articlePages = ArticlePagesGenerator().generate();
+
+  });
+});
+
+*/
