4f677475e271f6a76ff94526a44c6914d3aee9ec
JonayGodoy <jonay.godoy.reyes@gmail.com>
2019-01-07 22:54:35 +0100

    add gh-page

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ export default {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].[contenthash].js'
   },
   plugins: [
